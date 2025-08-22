fb2f79c3804e163bd4e5ec44c85965f9006d7d01
varun99015 <153973261+varun99015@users.noreply.github.com>
2025-08-22 22:13:17 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base:'/',
+  // For production build, ensures paths are correct
+  base: '/', 
+  // For local development, proxies API calls to your backend
   server: {
     proxy: {
-      // Any request starting with /api will be forwarded to your backend
       '/api': 'http://localhost:5000' 
     }
   }
-});
-
+})
