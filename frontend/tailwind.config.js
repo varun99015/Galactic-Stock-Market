f87fa938bb98a53487c5ab75d3784328f73de431
varun99015 <153973261+varun99015@users.noreply.github.com>
2025-08-22 22:48:07 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"];
-export const theme = {
-  extend: {
-    fontFamily: {
-      orbitron: ["Orbitron", "sans-serif"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        orbitron: ["Orbitron", "sans-serif"],
+      },
     },
   },
   plugins: [],
-};
-
+}
